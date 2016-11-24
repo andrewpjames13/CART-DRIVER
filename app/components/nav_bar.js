8d04183a115032b73ce57bfb231a2587d833d4b4
Andrew James <andrewpjames87@gmail.com>
2016-11-24 10:52:30 -0700

    Add active states and prepare navigation for scroll

--- app/components/nav_bar.js
+++ app/components/nav_bar.js
@@ -6,24 +6,32 @@ export default class NavBar extends Component {
   render() {
     return(
       <div className="nav-bar">
-        <a href="#" className="tiny-20 nav-item" onClick={this.onNavBarItemClick}>Home</a>
-        <a href="#" className="tiny-20 nav-item" onClick={this.onNavBarItemClick}>Food</a>
+        <a href="#home" className="tiny-20 nav-item active" onClick={this.onNavBarItemClick}>Home</a>
+        <a href="#food" className="tiny-20 nav-item" onClick={this.onNavBarItemClick}>Food</a>
         <a href="#" className="tiny-20 nav-item" onClick={this.onNavBarItemClick}>
-          <div className="call-me">call</div>
+          <div className="call-me"></div>
         </a>
-        <a href="#" className="tiny-20 nav-item" onClick={this.onNavBarItemClick}>Photos</a>
-        <a href="#" className="tiny-20 nav-item" onClick={this.onNavBarMenuClick}>Menu</a>
-        <a href="#" className="tiny-33 nav-item" onClick={this.onNavBarItemClick}>PRESS</a>
-        <a href="#" className="tiny-33 nav-item" onClick={this.onNavBarItemClick}>ABOUT</a>
-        <a href="#" className="tiny-33 nav-item" onClick={this.onNavBarItemClick}>CONTACT</a>
+        <a href="#photos" className="tiny-20 nav-item" onClick={this.onNavBarItemClick}>Photos</a>
+        <a href="#menu" className="tiny-20 nav-item" onClick={this.onNavBarMenuClick}>Menu</a>
+        <div className="tiny-100 border-top">
+          <a href="#press" className="tiny-33 nav-item" onClick={this.onNavBarItemClick}>PRESS</a>
+          <a href="#about" className="tiny-33 nav-item border-left-right" onClick={this.onNavBarItemClick}>ABOUT</a>
+          <a href="#contct" className="tiny-33 nav-item" onClick={this.onNavBarItemClick}>CONTACT</a>
+        </div>
       </div>
     );
   }
 
   onNavBarItemClick(event) {
     event.preventDefault();
-    // scroll to section
-  }
+    $('.nav-item').removeClass('active');
+    $(event.target).addClass('active');
+    // $('html,body').animate({
+    //   scrollTop: $( $(this).attr('href') ).offset().top
+    // }, 800);
+    // return false;
+  };
+
 
   onNavBarMenuClick(event) {
     event.preventDefault();
