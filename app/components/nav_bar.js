9de2dba8554493a5ec515115fc433eec989c31f3
Andrew James <andrewpjames87@gmail.com>
2016-11-25 12:42:38 -0700

    Updated navBar

--- app/components/nav_bar.js
+++ app/components/nav_bar.js
@@ -6,7 +6,15 @@ export default class NavBar extends Component {
   render() {
     return(
       <div className="nav-bar">
-        <a href="#home" className="tiny-20 nav-item active" onClick={this.onNavBarItemClick}>Home</a>
+        <a href="#home" className="tiny-20 nav-item active">
+          <div className="svg-container">
+            <svg version="1.1" className="home-icon" x="0px" y="0px" viewBox="0 0 24 24">
+              <path d="M24,12.1l-1.4,1.5L12,3.7L1.4,13.6L0,12.1L12,1C12,1,24,12.1,24,12.1z M20,13.9L18,24H6L4,13.9l8-7.4
+              	C12,6.5,20,13.9,20,13.9z M13,20c0-0.6-0.4-1-1-1s-1,0.4-1,1s0.4,1,1,1S13,20.6,13,20z M14,15c0-1.1-0.9-2-2-2s-2,0.9-2,2s0.9,2,2,2
+              S14,16.1,14,15z"/>
+            </svg>
+          </div>
+        </a>
         <a href="#food" className="tiny-20 nav-item" onClick={this.onNavBarItemClick}>Food</a>
         <a href="#" className="tiny-20 nav-item" onClick={this.onNavBarItemClick}>
           <div className="call-me"></div>
@@ -23,15 +31,16 @@ export default class NavBar extends Component {
   }
 
   onNavBarItemClick(event) {
+    console.log(event);
     event.preventDefault();
-    $('.nav-item').removeClass('active');
+    $('.nav-item, .svg-container, .svg-container svg').removeClass('active');
     $(event.target).addClass('active');
     // $('html,body').animate({
     //   scrollTop: $( $(this).attr('href') ).offset().top
     // }, 800);
     // return false;
   };
-
+  
 
   onNavBarMenuClick(event) {
     event.preventDefault();
