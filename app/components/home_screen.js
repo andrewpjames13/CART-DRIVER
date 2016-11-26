9e970ba605440d20cd1372bdc7187b3a378fae4d
Andrew James <andrewpjames87@gmail.com>
2016-11-26 11:53:10 -0700

    Update styling for home screen content

--- app/components/home_screen.js
+++ app/components/home_screen.js
@@ -3,7 +3,19 @@ import React, { Component } from 'react';
 
 const HomeScreen = (props) => {
   return (
-    <div className="home-container" style={{'backgroundImage': 'url(images/cart-driver-oven-pizza.jpg)'}}></div>
+    <div className="home-container" style={{'backgroundImage': 'url(images/cart-driver-oven-pizza.jpg)'}}>
+      <div className="tiny-100 home-content">
+        <h1 className="bold home-title">CART-DRIVER</h1>
+        <h6>CLOSES IN</h6>
+        <h1 className="bold clock-numbers">5</h1>
+        <h3 className="clock-letters">HR</h3>
+        <h1 className="bold clock-numbers">32</h1>
+        <h3 className="clock-letters">MIN</h3>
+        <h1 className="bold clock-numbers">45</h1>
+        <h3 className="clock-letters">SEC</h3>
+        <h6 className="bold green">OPEN 12PM - 12AM SEVEN DAYS A WEEK</h6>
+      </div>
+    </div>
   );
 };
 
