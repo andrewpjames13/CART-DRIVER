2b68dbff2951e1750d00895fa426b86344432838
Andrew James <andrewpjames87@gmail.com>
2016-11-25 23:16:30 -0700

    Add open state to navBar

--- app/components/nav_bar.js
+++ app/components/nav_bar.js
@@ -6,20 +6,35 @@ class NavBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { activeSection: 'home' };
+    this.state = {
+      activeSection: 'home',
+      open: false
+    };
   }
 
   handleClick(section) {
-    this.setState({ activeSection: section });
+    if (section === 'nav-menu' && this.state.open) {
+      this.setState({open: false});
+    } else if (section === 'nav-menu') {
+      this.setState({open: true});
+    } else {
+      this.setState({ activeSection: section });
+    }
   }
 
   render() {
+    var classNames = require('classnames');
+    var listClasses = classNames({
+      'nav-bar': true,
+      'open': this.state.open
+    });
+
     const navBarItems = this.props.sections.map((section) => {
       return <NavBarItem key={section.name} section={section} onClick={this.handleClick.bind(this)} activeSection={this.state.activeSection}/>
     });
 
     return (
-      <div className="nav-bar">
+      <div className={listClasses}>
         {navBarItems}
       </div>
     );
