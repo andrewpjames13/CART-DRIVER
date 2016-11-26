9bd83e7c527ba193794971fab24ee0f8852375e5
Andrew James <andrewpjames87@gmail.com>
2016-11-26 11:56:27 -0700

    standardize home font size to be h1 and h6

--- app/components/home_screen.js
+++ app/components/home_screen.js
@@ -8,11 +8,11 @@ const HomeScreen = (props) => {
         <h1 className="bold home-title">CART-DRIVER</h1>
         <h6>CLOSES IN</h6>
         <h1 className="bold clock-numbers">5</h1>
-        <h3 className="clock-letters">HR</h3>
+        <h6 className="clock-letters">HR</h6>
         <h1 className="bold clock-numbers">32</h1>
-        <h3 className="clock-letters">MIN</h3>
+        <h6 className="clock-letters">MIN</h6>
         <h1 className="bold clock-numbers">45</h1>
-        <h3 className="clock-letters">SEC</h3>
+        <h6 className="clock-letters">SEC</h6>
         <h6 className="bold green">OPEN 12PM - 12AM SEVEN DAYS A WEEK</h6>
       </div>
     </div>
