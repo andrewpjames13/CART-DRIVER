6fc75f5a99a1828cb98d52b09ffc520420230845
Andrew James <andrewpjames87@gmail.com>
2016-11-26 22:46:15 -0700

    Scroll event width sizing working pretty good

--- app/components/scroll_container.js
+++ app/components/scroll_container.js
@@ -1,10 +1,60 @@
 /*jshint esversion: 6 */
 import React, { Component } from 'react';
 
-const ScrollContainer = (props) => {
-  return (
-    <div className="scroll-container">Howdy</div>
-  );
+class ScrollContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      containerWidth: 90,
+      lastLocation: null
+    };
+  }
+
+  componentDidMount() {
+      window.addEventListener('scroll', this.handleScroll.bind(this));
+      // this.setState({offsetTop: document.querySelector('.scroll-container').offsetTop});
+  }
+
+  componentWillUnmount() {
+      window.removeEventListener('scroll', this.handleScroll.bind(this));
+  }
+
+  handleScroll(event) {
+    const innerHeight = window.innerHeight-100;
+    let currentLocation = event.srcElement.body.scrollTop;
+
+    if (currentLocation === 0) {
+      this.setState({
+        containerWidth: 90,
+        lastLocation: 1
+      });
+    } else if (this.state.lastLocation < currentLocation) {
+      if (currentLocation >= innerHeight) {
+        this.setState({
+          containerWidth: 100,
+          lastLocation: 99
+        });
+      } else {
+        this.setState({
+          containerWidth: this.state.containerWidth + 0.2,
+          lastLocation: currentLocation
+        });
+      }
+    } else if (this.state.lastLocation > currentLocation) {
+      this.setState({
+        containerWidth: this.state.containerWidth - 0.2,
+        lastLocation: currentLocation
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className="scroll-container" style={{'width': this.state.containerWidth + '%'}} >
+      </div>
+    );
+  }
 };
 
 export default ScrollContainer;
