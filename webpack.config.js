8468ffb337816a3590adce743fcd0131f286ac34
Andrew James <andrewpjames87@gmail.com>
2017-02-01 00:02:46 -0700

    Optimize production webpack more

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,19 @@ module.exports = {
           NODE_ENV: JSON.stringify('production')
         }
       }),
-      new webpack.optimize.UglifyJsPlugin()
+      new webpack.optimize.UglifyJsPlugin({
+        comments: false, // remove comments
+        compress: {
+          unused: true,
+          dead_code: true, // big one--strip code that will never execute
+          warnings: false, // good for prod apps so users can't peek behind curtain
+          drop_debugger: true,
+          conditionals: true,
+          evaluate: true,
+          drop_console: true, // strips console statements
+          sequences: true,
+          booleans: true,
+        }
+      })
     ]
 };
